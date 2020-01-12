8bf62df4b61edda8e839263ad14987a08aaa2411
Carlos <miklo.concepcion@gmail.com>
2020-01-12 14:12:21 -0600

    api working on port 9000. lets build a back end from scratch

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const server = require("./api/server");
 
 const host = process.HOST || "localhost";
-const port = proces.PORT || 9000;
+const port = process.PORT || 9000;
 
 server.listen(port, () => {
-  console.log(`LIstening on http://${host}:${port}`);
+  console.log(`Listening on http://${host}:${port}`);
 });
