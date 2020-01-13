34ba5729a27e9d187b71cfb8f1a5740bfa98d9d2
Carlos <miklo.concepcion@gmail.com>
2020-01-12 19:48:18 -0600

    re-formatted whole data flow and now going to probably have problems with dependencies. who knowslol. migration completed. onto seeding.

--- data/migrations/20200112182908_initial.js
+++ data/migrations/20200112182908_initial.js
@@ -1,14 +1,47 @@
 exports.up = async function(knex) {
   await knex.schema.createTable("recipes", table => {
     table.increments("id");
-    table.string("name").notNullable();
-    table.string("ingredients").notNullable();
-    table.string("instructions").notNullable();
+    table.text("name").notNullable();
   });
 
-  await knex.schema.createTable("ingredients", table => {});
+  await knex.schema.createTable("ingredients", table => {
+    table.increments("id");
+    table.text("name").notNullable();
+    // foreign key?
+  });
 
-  await knex.schema.createTable("instructions", table => {});
+  await knex.schema.createTable("instructions", table => {
+    table.increments("id");
+    table
+      .integer("instruction_number")
+      .unsigned()
+      .notNullable();
+    table.text("instructions").notNullable();
+    table
+      .integer("recipe_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("recipes")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE");
+  });
+  .createTable('carls-recipes', table => {
+      table.increments();
+      table.float('quantity').notNullable();
+      table.integer('recipe_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('recipes')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  })
 };
 
-exports.down = async function(knex) {};
+exports.down = async function(knex) {
+    await.knex.schema.dropTableIfExists('carls_recipes')
+    .dropTableIfExists('instructions')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes')
+};
