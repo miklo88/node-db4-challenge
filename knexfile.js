3da43a3fda84da9501ddbc8562412ebeccc80e20
Carlos <miklo.concepcion@gmail.com>
2020-01-12 18:49:38 -0600

    created dbconfig.js file and created a seeds folder with the recipes seed in it. moving onto creating the db file. slowly hooking it up

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,58 @@
 // Update with your config settings.
 
 module.exports = {
-
+  // development: {
+  //   client: "sqlite3",
+  //   connection: {
+  //     filename: "./dev.sqlite3"
+  //   },
+  //   // necessary when using sqlite3
+  //   useNullAsDefault: true
+  // },
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './dev.sqlite3'
+      filename: "./data/recipes.db3"
+    },
+    // necessary when using sqlite3
+    useNullAsDefault: true,
+    // generates migration files in a data/migrations
+    mirgrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   },
-
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations"
     }
   }
-
 };
