500bc0228724bfca87371754d6e13ea05cecf973
Carlos <miklo.concepcion@gmail.com>
2020-01-12 19:50:42 -0600

    cleaning up migrations folder. feeling good so far. knock on wood. remember you re-structurd your data check knex.js file meow.

--- data/migrations/20200112182908_initial.js
+++ data/migrations/20200112182908_initial.js
@@ -9,39 +9,41 @@ exports.up = async function(knex) {
     table.text("name").notNullable();
     // foreign key?
   });
-
-  await knex.schema.createTable("instructions", table => {
-    table.increments("id");
-    table
-      .integer("instruction_number")
-      .unsigned()
-      .notNullable();
-    table.text("instructions").notNullable();
-    table
-      .integer("recipe_id")
-      .unsigned()
-      .notNullable()
-      .references("id")
-      .inTable("recipes")
-      .onUpdate("CASCADE")
-      .onDelete("CASCADE");
-  });
-  .createTable('carls-recipes', table => {
+  await knex.schema
+    .createTable("instructions", table => {
+      table.increments("id");
+      table
+        .integer("instruction_number")
+        .unsigned()
+        .notNullable();
+      table.text("instructions").notNullable();
+      table
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    })
+    .createTable("carls-recipes", table => {
       table.increments();
-      table.float('quantity').notNullable();
-      table.integer('recipe_id')
-      .unsigned()
-      .notNullable()
-      .references('id')
-      .inTable('recipes')
-      .onUpdate('CASCADE')
-      .onDelete('CASCADE');
-  })
+      table.float("quantity").notNullable();
+      table
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
 };
 
 exports.down = async function(knex) {
-    await.knex.schema.dropTableIfExists('carls_recipes')
-    .dropTableIfExists('instructions')
-    .dropTableIfExists('ingredients')
-    .dropTableIfExists('recipes')
+  await knex.schema
+    .dropTableIfExists("carls_recipes")
+    .dropTableIfExists("instructions")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipes");
 };
