7e2eab4b62270df38eeea9eee7cde236994b7a9e
Carlos <miklo.concepcion@gmail.com>
2020-01-12 19:08:41 -0600

    creating migrations for my tables.

--- data/migrations/20200112182908_initial.js
+++ data/migrations/20200112182908_initial.js
@@ -1,8 +1,14 @@
+exports.up = async function(knex) {
+  await knex.schema.createTable("recipes", table => {
+    table.increments("id");
+    table.string("name").notNullable();
+    table.string("ingredients").notNullable();
+    table.string("instructions").notNullable();
+  });
 
-exports.up = function(knex) {
-  
+  await knex.schema.createTable("ingredients", table => {});
+
+  await knex.schema.createTable("instructions", table => {});
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = async function(knex) {};
--- data/seeds/001-recipes.js
+++ data/seeds/001-recipes.js
@@ -1,13 +1,26 @@
-
-exports.seed = function(knex) {
+exports.seed = function(knex, Promise) {
+  // // Deletes ALL existing entries
+  // return knex('table_name').del()
+  //   .then(function () {
+  //     // Inserts seed entries
+  //     return knex('table_name').insert([
+  //       {id: 1, colName: 'rowValue1'},
+  //       {id: 2, colName: 'rowValue2'},
+  //       {id: 3, colName: 'rowValue3'}
+  //     ]);
+  //   });
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("recipes")
+    .truncate()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("recipes").insert([
+        { id: 1, name: "recipeName", ingredients: "" },
+        { id: 2, name: "recipeName" },
+        { id: 3, name: "recipeName" }
+
+        // {id: 2, colName: 'rowValue2'},
+        // {id: 3, colName: 'rowValue3'}
       ]);
     });
 };
